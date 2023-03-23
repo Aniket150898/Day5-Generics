8994237c5dcbd6fa5146657b2a6f45f6c9229b6a
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-23 13:17:17 +0530

    [Aniket]- Deleteing index element of Array Using Class Generics

---
+++ ClassGenerics/DeleteArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassGenerics
+{
+    class DeleteArray<T>
+    {
+        public void Delete(ref T[] arr, T item)
+        {
+            // Find the index of the item to delete
+            int index = Array.IndexOf(arr, item);
+
+            if (index >= 0)
+            {
+                // If the item is found, create a new array with one less element
+                T[] newArr = new T[arr.Length - 1];
+
+                // Copy the elements before the item to the new array
+                Array.Copy(arr, 0, newArr, 0, index);
+
+                // Copy the elements after the item to the new array
+                Array.Copy(arr, index + 1, newArr, index, arr.Length - index - 1);
+
+                // Update the reference to the original array
+                arr = newArr;
+            }
+        }
+    }
+}
---
+++ ClassGenerics/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perform Class Generics");
+            // Create an array with a mix of integer, double, and char elements
+            object[] arr = new object[] { 1, 2.0, 'a', 3, 4.0, 'b' };
+
+            // Print the original array
+            Console.WriteLine("Original array: " + string.Join(", ", arr));
+
+            // Create a DeleteArray instance that can handle objects
+            DeleteArray<object> deleteObj = new DeleteArray<object>();
+
+            // Delete the 'a' character from the array
+            deleteObj.Delete(ref arr, 2.0);
+
+            // Print the modified array
+            Console.WriteLine("Array after deleting 2.0: " + string.Join(", ", arr));
+
+            // Delete the 'b' character from the array
+            deleteObj.Delete(ref arr, 'b');
+
+            // Print the final array
+            Console.WriteLine("Array after deleting 'b': " + string.Join(", ", arr));
+        }
+    }
+}
