65a81eb844455bbb190e70a214a3e8020cc6a5a7
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-23 12:32:28 +0530

    [Aniket]- Deleteing one index element from Different Arrays

---
+++ Generics/DeleteArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class DeleteArray
+    {
+        public static void DeleteMethod(ref int[] arr, int index)
+        {
+            for (int i = index; i < arr.Length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            Array.Resize(ref arr, arr.Length - 1);
+        }
+
+        public static void DeleteMethod(ref double[] arr, int index)
+        {
+            for (int i = index; i < arr.Length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            Array.Resize(ref arr, arr.Length - 1);
+        }
+
+        public static void DeleteMethod(ref char[] arr, int index)
+        {
+            for (int i = index; i < arr.Length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            Array.Resize(ref arr, arr.Length - 1);
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Everyone,Performing Class, Method Generics Problems");
+            //Defined Array
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+
+            Console.WriteLine("Original arrays:");
+            Console.WriteLine(string.Join(",", intArray));
+            Console.WriteLine(string.Join(",", doubleArray));
+            Console.WriteLine(string.Join(",", charArray));
+
+            DeleteArray.DeleteMethod(ref intArray, 2);
+            DeleteArray.DeleteMethod(ref doubleArray, 3);
+            DeleteArray.DeleteMethod(ref charArray, 4);
+
+            Console.WriteLine("Arrays after deleting element at index 2, 3, and 4:");
+            Console.WriteLine(string.Join(",", intArray));
+            Console.WriteLine(string.Join(",", doubleArray));
+            Console.WriteLine(string.Join(",", charArray));
+
+            Console.ReadLine();
         }
     }
 }
